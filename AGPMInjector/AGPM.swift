e95bdff684930d455ef788f364272f173e5b47c1
Pavo <brockbackups@gmail.com>
2018-12-28 23:12:03 -0500

    add plist layout

---
+++ AGPMInjector/AGPM.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+
+struct GetAGPMInfo: Codable {
+    var BuildMachineOSBuild: String
+    var CFBundleDevelopmentRegion: String
+    var CFBundleGetInfoString: String
+    var CFBundleIdentifier: String
+    var CFBundleInfoDictionaryVersion: String
+    var CFBundleName: String
+    var CFBundlePackageType: String
+    var CFBundleShortVersionString : String
+    var CFBundleSignature: String
+    var CFBundleVersion: String
+    var NSHumanReadableCopyright: String
+    var OSBundleRequired: String
+    var IOKitPersonalities: IOKitPersonalities
+    
+    struct IOKitPersonalities: Codable {
+        var AGPM: AGPM
+    }
+    
+    struct AGPM: Codable {
+        var CFBundleIdentifier: String
+        var IOClass: String
+        var IONameMatch: String
+        var IOProviderClass: String
+        
+        
+    }
+}
+
