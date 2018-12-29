17870f71543aaf82114bbaf05e1d0ea5301dd981
Pavo <brockbackups@gmail.com>
2018-12-29 12:30:54 -0500

    Fixed struct order

--- AGPMInjector/AGPM.swift
+++ AGPMInjector/AGPM.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 struct GetAGPMInfo: Codable {
     var BuildMachineOSBuild: String
     var CFBundleDevelopmentRegion: String
@@ -9,24 +8,21 @@ struct GetAGPMInfo: Codable {
     var CFBundleInfoDictionaryVersion: String
     var CFBundleName: String
     var CFBundlePackageType: String
-    var CFBundleShortVersionString : String
+    var CFBundleShortVersionString: String
     var CFBundleSignature: String
     var CFBundleVersion: String
     var NSHumanReadableCopyright: String
-    var OSBundleRequired: String
     var IOKitPersonalities: IOKitPersonalities
-    
-    struct IOKitPersonalities: Codable {
-        var AGPM: AGPM
-    }
-    
-    struct AGPM: Codable {
-        var CFBundleIdentifier: String
-        var IOClass: String
-        var IONameMatch: String
-        var IOProviderClass: String
-        
-        
-    }
+    var OSBundleRequired: String
 }
 
+struct IOKitPersonalities: Codable {
+    var AGPM: AGPM
+}
+
+struct AGPM: Codable {
+    var CFBundleIdentifier: String
+    var IOClass: String
+    var IONameMatch: String
+    var IOProviderClass: String
+}
