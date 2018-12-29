ea56061dba2451eccd63499bf5dbc8ae82cb7976
Pavo <brockbackups@gmail.com>
2018-12-28 22:35:28 -0500

    Initial Commit

---
+++ AGPMInjector/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AGPMInjector
+//
+//  Created by Henry Brock on 12/28/18.
+//  Copyright © 2018 Pavo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
