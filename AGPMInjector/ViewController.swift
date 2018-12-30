3396484c856431c179541798e18a03d5177270d2
Pavo <brockbackups@gmail.com>
2018-12-29 19:37:38 -0500

    Re-structuring

--- AGPMInjector/ViewController.swift
+++ AGPMInjector/ViewController.swift
@@ -26,18 +26,6 @@ class ViewController: NSViewController {
                 let tDocumentDirectory = fileManager.urls(for: .desktopDirectory, in: .userDomainMask).first!
                 let filePath =  tDocumentDirectory.appendingPathComponent("\(setAGPMInjectorDirectory)")
                 do {
-//                    let bundleID = "com.apple.driver.toledaAGPM"
-//                    let bundleName = "toledaAGPM"
-//                    let bundleShortVersionName = "2.0.0-toledaAGPM"
-//                    let buildMachine = "14A100"
-//                    let region = "English"
-//                    let infoString = "Copyright © 2014 Apple All rights reserved."
-//                    let dictVersion = "6.0"
-//                    let packageType = "KEXT"
-//                    let bundleSig = "????"
-//                    let bundleVersion = "1.0.0"
-//                    let copyright = "Copyright © 2014 Apple. All rights reserved."
-//                    let bundleRequire = "Local-Root"
                     let data = try Data(contentsOf: getAGPMFilePathURL)
                     let decoder = PropertyListDecoder()
                     let AGPMListData = try decoder.decode(GetAGPMInfo.self, from: data)
