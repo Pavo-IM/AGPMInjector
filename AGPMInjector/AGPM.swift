664ceb25e489ffaf58522295473308322c4e06fe
Pavo <brockbackups@gmail.com>
2019-01-02 15:52:52 -0500

    Fixed PlistGet Parsing

--- AGPMInjector/AGPM.swift
+++ AGPMInjector/AGPM.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Plist: Codable {
+struct PlistGet: Codable {
     let buildMachineOSBuild, cfBundleDevelopmentRegion, cfBundleGetInfoString, cfBundleIdentifier: String
     let cfBundleInfoDictionaryVersion, cfBundleName, cfBundlePackageType, cfBundleShortVersionString: String
     let cfBundleSignature, cfBundleVersion, nsHumanReadableCopyright: String
@@ -22,63 +22,111 @@ struct Plist: Codable {
         case ioKitPersonalities = "IOKitPersonalities"
         case osBundleRequired = "OSBundleRequired"
     }
-}
-
-struct IOKitPersonalities: Codable {
-    let agpm: Agpm
     
-    enum CodingKeys: String, CodingKey {
-        case agpm = "AGPM"
+    struct IOKitPersonalities: Codable {
+        let agpm: Agpm
+        
+        enum CodingKeys: String, CodingKey {
+            case agpm = "AGPM"
+        }
+        
+        struct Agpm: Codable {
+            let cfBundleIdentifier, ioClass, ioNameMatch, ioProviderClass: String
+            let machines: [String: Machines]
+            
+            enum CodingKeys: String, CodingKey {
+                case cfBundleIdentifier = "CFBundleIdentifier"
+                case ioClass = "IOClass"
+                case ioNameMatch = "IONameMatch"
+                case ioProviderClass = "IOProviderClass"
+                case machines = "Machines"
+            }
+            
+            struct Machines: Codable {
+            }
+        }
     }
 }
 
-struct Agpm: Codable {
-    let cfBundleIdentifier, ioClass, ioNameMatch, ioProviderClass: String
-    let machines: Machines
+struct PlistSet: Codable {
+    let buildMachineOSBuild, cfBundleDevelopmentRegion, cfBundleGetInfoString, cfBundleIdentifier: String
+    let cfBundleInfoDictionaryVersion, cfBundleName, cfBundlePackageType, cfBundleShortVersionString: String
+    let cfBundleSignature, cfBundleVersion, nsHumanReadableCopyright: String
+    let ioKitPersonalities: IOKitPersonalities
+    let osBundleRequired: String
     
     enum CodingKeys: String, CodingKey {
+        case buildMachineOSBuild = "BuildMachineOSBuild"
+        case cfBundleDevelopmentRegion = "CFBundleDevelopmentRegion"
+        case cfBundleGetInfoString = "CFBundleGetInfoString"
         case cfBundleIdentifier = "CFBundleIdentifier"
-        case ioClass = "IOClass"
-        case ioNameMatch = "IONameMatch"
-        case ioProviderClass = "IOProviderClass"
-        case machines = "Machines"
+        case cfBundleInfoDictionaryVersion = "CFBundleInfoDictionaryVersion"
+        case cfBundleName = "CFBundleName"
+        case cfBundlePackageType = "CFBundlePackageType"
+        case cfBundleShortVersionString = "CFBundleShortVersionString"
+        case cfBundleSignature = "CFBundleSignature"
+        case cfBundleVersion = "CFBundleVersion"
+        case nsHumanReadableCopyright = "NSHumanReadableCopyright"
+        case ioKitPersonalities = "IOKitPersonalities"
+        case osBundleRequired = "OSBundleRequired"
     }
-}
-
-struct Machines: Codable {
-    let macPro61: macPro61
     
-    enum CodingKeys: String, CodingKey {
-        case macPro61 = "MacPro6,1"
-    }
-}
-
-struct macPro61: Codable {
-    let Gfx0: Gfx0
-    
-    enum CodingKeys: String, CodingKey {
-        case Gfx0 = "GFX0"
-    }
-}
-
-struct Gfx0: Codable {
-    let agdcEnabled: Int
-    let heuristic: Heuristic
-    let maxPowerState, minPowerState, controlID: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case agdcEnabled = "AGDCEnabled"
-        case heuristic = "Heuristic"
-        case maxPowerState = "max-power-state"
-        case minPowerState = "min-power-state"
-        case controlID = "control-id"
-    }
-}
-
-struct Heuristic: Codable {
-    let id: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case id = "ID"
+    struct IOKitPersonalities: Codable {
+        let agpm: Agpm
+        
+        enum CodingKeys: String, CodingKey {
+            case agpm = "AGPM"
+        }
+        
+        struct Agpm: Codable {
+            let cfBundleIdentifier, ioClass, ioNameMatch, ioProviderClass: String
+            let machines: Machines
+            
+            enum CodingKeys: String, CodingKey {
+                case cfBundleIdentifier = "CFBundleIdentifier"
+                case ioClass = "IOClass"
+                case ioNameMatch = "IONameMatch"
+                case ioProviderClass = "IOProviderClass"
+                case machines = "Machines"
+            }
+            
+            struct Machines: Codable {
+                let macPro61: macPro61
+                
+                enum CodingKeys: String, CodingKey {
+                    case macPro61 = "MacPro6,1"
+                }
+                
+                struct macPro61: Codable {
+                    let Gfx0: Gfx0
+                    
+                    enum CodingKeys: String, CodingKey {
+                        case Gfx0 = "GFX0"
+                    }
+                    
+                    struct Gfx0: Codable {
+                        let agdcEnabled: Int
+                        let heuristic: Heuristic
+                        let maxPowerState, minPowerState, controlID: Int
+                        
+                        enum CodingKeys: String, CodingKey {
+                            case agdcEnabled = "AGDCEnabled"
+                            case heuristic = "Heuristic"
+                            case maxPowerState = "max-power-state"
+                            case minPowerState = "min-power-state"
+                            case controlID = "control-id"
+                        }
+                        
+                        struct Heuristic: Codable {
+                            let id: Int
+                            
+                            enum CodingKeys: String, CodingKey {
+                                case id = "ID"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- AGPMInjector/ViewController.swift
+++ AGPMInjector/ViewController.swift
@@ -16,39 +16,38 @@ class ViewController: NSViewController {
         // Get the URL of the Info.plist path from AppleGraphicsPowerManagement.kext.
         let getAGPMFilePath = "/System/Library/Extensions/AppleGraphicsPowerManagement.kext/Contents/Info.plist"
         let getAGPMFilePathURL = URL.init(fileURLWithPath: getAGPMFilePath)
+        
+        // Create Decoder and Encoder objects
+        let plistDecoder = PropertyListDecoder()
+        let plistEncoder = PropertyListEncoder()
+        plistEncoder.outputFormat = .xml
 
+        // Decoder the AppleGraphicsPowerManagement.kext Info.plist and get some information to save as variable
+        let data = try! Data(contentsOf: getAGPMFilePathURL)
+        let plistData = try! plistDecoder.decode(PlistGet.self, from: data)
+        
         
         // Write the AGPMInjector.kext/Contents directory to the users Desktop and copying AGPMInjector.plist into that directory as Info.plist
         let setAGPMInjectorDirectory = "AGPMInjector.kext/Contents"
         let setInforPlistLocation = "Info.plist"
         let fileManager = FileManager.default
-        let setAGPMInjectorPath = Bundle.main.url(forResource: "AGPMInjector", withExtension: "plist")!
         let tDocumentDirectory = fileManager.urls(for: .desktopDirectory, in: .userDomainMask).first!
         let filePath =  tDocumentDirectory.appendingPathComponent("\(setAGPMInjectorDirectory)")
-        
-        
+
         // Set the default properties of the root section of the plist
         let bundleID = "com.apple.driver.AGPMInjector"
         let bundleName = "AGPMInjector"
         let bundleShortVersionName = "1.0-AGPMInjector"
-        let buildMachine = "14A100"
-        let region = "English"
-        let infoString = "Copyright © 2014 Apple All rights reserved."
-        let dictVersion = "6.0"
-        let packageType = "KEXT"
         let bundleSig = "????"
-        let bundleVersion = "1.0.0"
-        let copyright = "Copyright © 2014 Apple. All rights reserved."
-        let bundleRequire = "Local-Root"
         
-        
+        // Create a object to represent the plist data to get encoded
+        let testEncoding = PlistSet(buildMachineOSBuild: plistData.buildMachineOSBuild, cfBundleDevelopmentRegion: plistData.cfBundleDevelopmentRegion, cfBundleGetInfoString: plistData.cfBundleGetInfoString, cfBundleIdentifier: bundleID, cfBundleInfoDictionaryVersion: plistData.cfBundleInfoDictionaryVersion, cfBundleName: bundleName, cfBundlePackageType: plistData.cfBundlePackageType, cfBundleShortVersionString: bundleShortVersionName, cfBundleSignature: bundleSig, cfBundleVersion: plistData.cfBundleVersion, nsHumanReadableCopyright: plistData.nsHumanReadableCopyright, ioKitPersonalities: PlistSet.IOKitPersonalities(agpm: PlistSet.IOKitPersonalities.Agpm(cfBundleIdentifier: plistData.ioKitPersonalities.agpm.cfBundleIdentifier, ioClass: plistData.ioKitPersonalities.agpm.ioClass, ioNameMatch: plistData.ioKitPersonalities.agpm.ioNameMatch, ioProviderClass: plistData.ioKitPersonalities.agpm.ioProviderClass, machines: PlistSet.IOKitPersonalities.Agpm.Machines(macPro61: PlistSet.IOKitPersonalities.Agpm.Machines.macPro61(Gfx0: PlistSet.IOKitPersonalities.Agpm.Machines.macPro61.Gfx0(agdcEnabled: 1, heuristic: PlistSet.IOKitPersonalities.Agpm.Machines.macPro61.Gfx0.Heuristic(id: -1), maxPowerState: 15, minPowerState: 0, controlID: 1))))), osBundleRequired: plistData.osBundleRequired)
+
         do {
             // Encode the Plist properties and write it to the Info.plist file being saved to the current logged in user's desktop
             try fileManager.createDirectory(atPath: filePath.path, withIntermediateDirectories: true, attributes: nil)
             let InfoPlistfilePath =  filePath.appendingPathComponent("\(setInforPlistLocation)")
-            let encoder = PropertyListEncoder()
-            encoder.outputFormat = .xml
-            let dataSet = try encoder.encode(someSettings)
+            let dataSet = try plistEncoder.encode(testEncoding)
             try dataSet.write(to: InfoPlistfilePath)
         } catch {
             print(error)
