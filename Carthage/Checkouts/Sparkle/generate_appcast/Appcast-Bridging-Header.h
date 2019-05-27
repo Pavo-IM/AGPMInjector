b1677a08d680af5a62764c5acf4ec27dd3ac050d
Pavo <brockbackups@gmail.com>
2019-05-27 10:16:03 -0400

    Removed Carthage folder from repo

--- Carthage/Checkouts/Sparkle/generate_appcast/Appcast-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#import "SUStandardVersionComparator.h"
-#import "SUConstants.h"
-#import "SUErrors.h"
-#import "SUUnarchiver.h"
-#import "SUBinaryDeltaUnarchiver.h"
-#import "SUBinaryDeltaCreate.h"
